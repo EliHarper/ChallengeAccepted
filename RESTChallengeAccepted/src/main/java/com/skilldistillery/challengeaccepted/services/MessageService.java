61e88963aee6e95a1270e2f4aee12136eb75baf1
Dora Harper <doraeharper@gmail.com>
2018-06-01 17:10:42 -0600

    querys for message threads

--- RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/MessageService.java
+++ RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/MessageService.java
@@ -6,10 +6,11 @@ import com.skilldistillery.challengeaccepted.entities.Message;
 
 public interface MessageService {
 	
-	public Set<Message> index(String username);
+	public Set<Message> indexThreads(int id);
 	public Message show(String username, int mid);
 	public Message create(Message message);
 //	public Message update(String username, int mid, Message message);
 	public void destroy (String username, int mid);
 
+	public Set<Message> indexThreadMessages(int tid);
 }
