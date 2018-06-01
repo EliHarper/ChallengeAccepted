75a32e568b554c8295b133eb5ec5404a1fce4b5f
EliHarper <eli.harper1995@gmail.com>
2018-06-01 15:41:45 -0600

    Finished UserController

--- RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/UserService.java
+++ RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/UserService.java
@@ -1,5 +1,15 @@
 package com.skilldistillery.challengeaccepted.services;
 
-public interface UserService {
+import java.util.List;
 
+import com.skilldistillery.challengeaccepted.entities.User;
+
+public interface UserService {
+	
+	public User create(User u);
+	public User update(User u);
+	public List <User> index();
+	public User show(int id);
+	public Boolean delete(int id);
+	
 }
