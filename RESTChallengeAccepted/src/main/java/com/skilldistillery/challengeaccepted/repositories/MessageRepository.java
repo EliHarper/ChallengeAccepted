7f1486f6b7334c26cb1fe954052a1aa9f6c3c050
miiaevia <miiaevia@gmail.com>
2018-06-02 11:38:09 -0600

    gradle change

--- RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/repositories/MessageRepository.java
+++ RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/repositories/MessageRepository.java
@@ -12,11 +12,11 @@ public interface MessageRepository extends JpaRepository<Message, Integer> {
 
 	public Set<Message> findBySenderUsername(String username);
 	public Set<Message> findByReceiverUsername(String username);
-	public Message findByIdAndUserUsername(int id, String username);
+//	public Message findByIdAndUserUsername(int id, String username);
 	
-	@Query("SELECT * FROM message m JOIN user s ON s.id = m.sender_id JOIN user r ON r.id = m.receiver_id WHERE m.id = m.thread_id AND (s.id = :id || r.id = :id)")
-	public Set<Message> getMessageThreadByUserId(@Param("id") int id);
-	
-	public Set<Message> findByThreadId(int tid);
+//	@Query("SELECT m FROM Message m JOIN User s ON s.id = m.sender.id JOIN User r ON r.id = m.receiver.id WHERE m.id = m.threadId AND (s.id = :id OR r.id = :id)")
+//	public Set<Message> getMessageThreadByUserId(@Param("id") int id);
+//	
+//	public Set<Message> findByThreadId(int tid);
 	
 }
