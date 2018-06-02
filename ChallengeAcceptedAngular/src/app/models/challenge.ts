0e35812e7525a1ea0538000ed6d23c10e48afa01
AlexWagner <alexwagner1990@verizon.net>
2018-06-02 11:36:49 -0600

    Angular app.module config stuff

--- ChallengeAcceptedAngular/src/app/models/challenge.ts
+++ ChallengeAcceptedAngular/src/app/models/challenge.ts
@@ -1,2 +1,17 @@
 export class Challenge {
+  id: number;
+  location: string;
+  name: string;
+  description: string;
+  wager: number;
+  minNumberOfChallengers: number;
+  maxNumberOfChallengers: number;
+  status: {};
+  acceptors: [{}];
+  tags: [{}];
+  creator: {};
+  skill: {};
+  userChallenges: [{}];
+  timeCreated: Date;
+
 }
