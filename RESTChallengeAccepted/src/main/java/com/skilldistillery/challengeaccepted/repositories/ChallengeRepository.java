d8b6581ae9f970eccb92f2e6e8be4e630f9b1183
miiaevia <miiaevia@gmail.com>
2018-06-04 10:34:52 -0600

    pushing changes

--- RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/repositories/ChallengeRepository.java
+++ RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/repositories/ChallengeRepository.java
@@ -1,9 +1,17 @@
 package com.skilldistillery.challengeaccepted.repositories;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.challengeaccepted.entities.Challenge;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Integer> {
+	
+	// find all challenges by status, passing status id
+	public Set<Challenge> findByStatus(int sid);
+	
+	// find all challenges by status for a single user, passing status id and username
+	public Set<Challenge> findByStatusAndUserUsername(int sid, String username);
 
 }
--- RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/ChallengeServiceImpl.java
+++ RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/ChallengeServiceImpl.java
@@ -1,30 +1,20 @@
 package com.skilldistillery.challengeaccepted.services;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.challengeaccepted.entities.Challenge;
 import com.skilldistillery.challengeaccepted.repositories.ChallengeRepository;
-import com.skilldistillery.challengeaccepted.repositories.SkillRepository;
-import com.skilldistillery.challengeaccepted.repositories.TagRepository;
-import com.skilldistillery.challengeaccepted.repositories.UserRepository;
 
 @Service
 public class ChallengeServiceImpl implements ChallengeService {
 
 	@Autowired
 	private ChallengeRepository chaRepo;
-	
-	@Autowired
-	private UserRepository userRepo;
-	
-	@Autowired
-	private SkillRepository skillRepo;
-	
-	@Autowired
-	private TagRepository tagRepo;
+
 	
 	public Challenge create(Challenge c) {
 		chaRepo.saveAndFlush(c);
@@ -53,5 +43,15 @@ public class ChallengeServiceImpl implements ChallengeService {
 			e.printStackTrace();
 			return false;
 		}
+	}
+	
+	// view all challenges by status id
+	public Set<Challenge> indexStatusChallenges (int sid) {
+		return chaRepo.findByStatus(sid);
+	}
+	
+	//view all challenges for a user by status id
+	public Set<Challenge> indexStatusChallengesByUser(int sid, int uid) {
+		return chaRepo.findByStatusAndUserUsername(sid, username);
 	}
 }
