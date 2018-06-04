41c13b3a0021fbbe985aad4e3fe263cb47b534e2
EliHarper <eli.harper1995@gmail.com>
2018-06-04 14:24:50 -0600

    Added logic for obtaining level of skill as well as progress to next level

--- ChallengeAcceptedAngular/src/app/user-skill.service.ts
+++ ChallengeAcceptedAngular/src/app/user-skill.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { UserSkill } from './models/user-skill';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserSkillService {
 
-  constructor() { }
+  url = 'http://localhost:8080/api';
+
+  findUserSkillById(usid) {
+    return this.http.get<UserSkill>(`${this.url}/user/${usid}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(err);
+      })
+    );
+  }
+
+  getLevelOfSkill(userSkill: UserSkill) {
+    return userSkill.points / 10;
+  }
+
+  getProgressToNextLvl(userSkill: UserSkill) {
+    return userSkill.points % 10;
+  }
+
+
+  constructor(private http: HttpClient) { }
 }
