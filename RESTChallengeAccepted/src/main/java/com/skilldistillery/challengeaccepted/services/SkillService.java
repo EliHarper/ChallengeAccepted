7ad41c98ed2313276a186fcdb98c759962246295
Dora Harper <doraeharper@gmail.com>
2018-06-04 11:24:41 -0600

    cleaned up skillz

--- RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/SkillService.java
+++ RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/SkillService.java
@@ -6,9 +6,6 @@ import com.skilldistillery.challengeaccepted.entities.Skill;
 
 public interface SkillService {
 
-	public Skill create(Skill s);
-	public Skill update(Skill s);
 	public List <Skill> index();
-	public Skill show(int id);
-	public Boolean delete(int id);
+
 }
