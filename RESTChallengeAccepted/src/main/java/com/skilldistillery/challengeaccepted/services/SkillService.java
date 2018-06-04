4c96fbba85312d1745676fb895126540a0fa970a
Dora Harper <doraeharper@gmail.com>
2018-06-04 11:22:12 -0600

    skill controller tested

--- RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/SkillService.java
+++ RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/services/SkillService.java
@@ -1,5 +1,14 @@
 package com.skilldistillery.challengeaccepted.services;
 
+import java.util.List;
+
+import com.skilldistillery.challengeaccepted.entities.Skill;
+
 public interface SkillService {
 
+	public Skill create(Skill s);
+	public Skill update(Skill s);
+	public List <Skill> index();
+	public Skill show(int id);
+	public Boolean delete(int id);
 }
