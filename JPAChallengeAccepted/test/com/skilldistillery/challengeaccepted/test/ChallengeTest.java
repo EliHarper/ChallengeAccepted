3bb975afdbf6a66fdd51efd9136181e4871ab00f
miiaevia <miiaevia@gmail.com>
2018-06-04 09:18:04 -0600

    added j-unit tests for challenge relationships

--- JPAChallengeAccepted/test/com/skilldistillery/challengeaccepted/test/ChallengeTest.java
+++ JPAChallengeAccepted/test/com/skilldistillery/challengeaccepted/test/ChallengeTest.java
@@ -35,4 +35,34 @@ public class ChallengeTest {
 	void test_challenge_associations() {
 		assertEquals("Drinking Challenge", c.getName());
 	}
+	
+	@Test
+	void test_challenge_has_a_status() {
+		assertEquals("active", c.getStatus().getName()); 
+	}
+	
+	@Test
+	void test_challenge_has_list_users() {
+		assertEquals("AlexTheDestroyer", c.getUsers().get(0).getUsername());
+	}
+	
+	@Test
+	void test_challenge_can_have_tags() {
+		assertEquals("bar games", c.getTags().get(0).getName());
+	}
+	
+	@Test
+	void test_challenge_has_creator() {
+		assertEquals("AlexTheDestroyer", c.getCreator().getUsername());
+	}
+	
+	@Test
+	void test_challenge_has_a_skill() {
+		assertEquals("drinking", c.getSkill().getName());
+	}
+	
+	@Test
+	void test_challenge_has_user_challenges() {
+		assertEquals(1, c.getUserChallenges().get(0).getId());
+	}
 }
