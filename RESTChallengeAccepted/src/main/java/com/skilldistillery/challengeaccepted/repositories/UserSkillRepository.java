f513096121dc5cae934f1b76a4c5058841c09183
miiaevia <miiaevia@gmail.com>
2018-06-05 08:17:12 -0600

    order user skills list by desc

--- RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/repositories/UserSkillRepository.java
+++ RESTChallengeAccepted/src/main/java/com/skilldistillery/challengeaccepted/repositories/UserSkillRepository.java
@@ -2,12 +2,15 @@ package com.skilldistillery.challengeaccepted.repositories;
 
 import java.util.Set;
 
+import javax.persistence.OrderBy;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.challengeaccepted.entities.UserSkill;
 
 public interface UserSkillRepository extends JpaRepository<UserSkill, Integer> {
 	
+	@OrderBy("points DESC")
 	public Set<UserSkill> findByUserId(int uid); 
 	
 	public UserSkill findById(int id);
