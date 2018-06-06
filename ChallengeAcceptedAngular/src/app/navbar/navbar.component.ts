378ab8835d43fbe07e0340ef646c066ae3eef172
Andrew Lyon <anlyon99@gmail.com>
2018-06-06 10:37:29 -0600

    added logic for creating new challenge

--- ChallengeAcceptedAngular/src/app/navbar/navbar.component.ts
+++ ChallengeAcceptedAngular/src/app/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
+import { Router } from '@angular/router';
+import { ChallengeViewComponent } from './../challenge-view/challenge-view.component';
 import { Challenge } from './../models/challenge';
 import { ChallengeService } from './../challenge.service';
 import { Component, OnInit } from '@angular/core';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -10,10 +13,16 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   challenge: Challenge = new Challenge();
 
+  createPokemon(challenge) {
+    this.challengeService.create(challenge).subscribe(
+      data => this.router.navigateByUrl('challview/' + data.id),
+      err => throwError(err)
+    );
+  }
 
 
-
-  constructor(private challengeService: ChallengeService) { }
+  constructor(private challengeService: ChallengeService,
+              private router: Router) { }
 
   ngOnInit() {
   }
